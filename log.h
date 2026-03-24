0d7a20517acea2d4a7b4a4b65a601d2b26618d97
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-03-24 10:07:49 +0100

    Add basic logging macros

---
+++ log.h
@@ -0,0 +1,16 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdio.h>
+
+typedef enum Log_types_t {
+    INFO,
+    ERROR
+} Log_types_t;
+
+#define stringify(x) _stringify_(x)
+#define _stringify_(x) #x
+#define LOG(type, msg) fprintf(stderr, "[%s]: FILE: %s LINE: %s; %s\n", #type, __FILE__, stringify(__LINE__), msg)
+
+#endif //LOG_H
+
