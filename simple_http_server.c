ca5240ff0ed1991d73182173fa5678d7af750417
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-05-02 06:01:44 +0200

    Move echoing request to separate function

--- simple_http_server.c
+++ simple_http_server.c
@@ -21,6 +21,23 @@ typedef struct Http_server_t {
     Input_queue_t* input_queue;
 } Http_server_t;
 
+void echo_request(Http_message_t* req) {
+    Request_line_t* req_line = (Request_line_t*)req->start_line;
+    printf("---req---echo---\n");
+    printf("%s %s %s\n",
+        http_method_to_string(req_line->method),
+        req_line->request_target,
+        req_line->http_version);
+    for (int i = 0; i < req->field_lines_count; ++i) {
+        printf("%s: %s\n", req->field_lines[i].field_name, req->field_lines[i].field_value);
+    }
+    printf("\n");
+    if (req->message_body) {
+        printf("%.*s\n", (int)req->body_size, req->message_body);
+        printf("\n");
+    }
+}
+
 int main() {
     signal(SIGCHLD, SIG_IGN);
     
@@ -40,53 +57,49 @@ int main() {
         }
     }
     
-    //Create in_stream reader
+    //Create reader
     s.input_queue = init_reader(s.tcp_connection.fd);
     
-    Http_message_t req;
-    init_http_message(&req, HTTP_REQUEST);
-    Http_status_t status = parse_http_request(&req, s.input_queue);
-    Request_line_t* req_line = (Request_line_t*)req.start_line;
-    printf("---req---echo---\n");
-    printf("%s %s %s\n",
-        http_method_to_string(req_line->method),
-        req_line->request_target,
-        req_line->http_version);
-    for (int i = 0; i < req.field_lines_count; ++i) {
-        printf("%s: %s\n", req.field_lines[i].field_name, req.field_lines[i].field_value);
-    }
-    printf("\n");
-    if (req.message_body) {
-        printf("%.*s\n", (int)req.body_size, req.message_body);
-        printf("\n");
-    }
-    Http_message_t res;
-    init_http_message(&res, HTTP_RESPONSE);
-    char status_char[4];
-    sprintf(status_char, "%d", status);
-    switch (status) {
-        case HTTP_STATUS_OK:
-            {
-                write_response_status_line(&res, "HTTP/1.1", status_char, (char*)http_status_to_string(status));
-                char* body = "<h1>Hello</h1>";
-                char size[1024];
-                sprintf(size, "%d", (int)strlen(body));
-                write_response_field_line(&res, "Content-length", size);
-                write_response_body_content_length(&res, body, (intmax_t)strlen(body));
-                send_response(s.tcp_connection, &res);
-            }
-            break;
-        default:
-            {
-                write_response_status_line(&res, "HTTP/1.1", status_char, (char*)http_status_to_string(status));
-                char* body = "<h1>default</h1>";
-                char size[1024];
-                sprintf(size, "%d", (int)strlen(body));
-                write_response_field_line(&res, "Content-length", size);
-                write_response_body_content_length(&res, body, (intmax_t)strlen(body));
-                send_response(s.tcp_connection, &res);
-            }
-            break;
+    //For now loop indefinitely
+    while (1) {
+        //Parse next request
+        Http_message_t req;
+        init_http_message(&req, HTTP_REQUEST);
+        Http_status_t status = parse_http_request(&req, s.input_queue);
+        
+        echo_request(&req);
+
+        //Prepare response
+        Http_message_t res;
+        init_http_message(&res, HTTP_RESPONSE);
+        char status_char[4];
+        sprintf(status_char, "%d", status);
+        switch (status) {
+            case HTTP_STATUS_OK:
+                {
+                    write_response_status_line(&res, "HTTP/1.1",
+                            status_char, (char*)http_status_to_string(status));
+                    char* body = "<h1>Hello</h1>";
+                    char size[1024];
+                    sprintf(size, "%d", (int)strlen(body));
+                    write_response_field_line(&res, "Content-length", size);
+                    write_response_body_content_length(&res, body, (intmax_t)strlen(body));
+                    send_response(s.tcp_connection, &res);
+                }
+                break;
+            default:
+                {
+                    write_response_status_line(&res, "HTTP/1.1",
+                            status_char, (char*)http_status_to_string(status));
+                    char* body = "<h1>default</h1>";
+                    char size[1024];
+                    sprintf(size, "%d", (int)strlen(body));
+                    write_response_field_line(&res, "Content-length", size);
+                    write_response_body_content_length(&res, body, (intmax_t)strlen(body));
+                    send_response(s.tcp_connection, &res);
+                }
+                break;
+        }
     }
 
     close_tcp_connection(&s.tcp_connection);
