e6a67f4624a4a578338ab4e521f49726c45f1a13
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-06-10 20:02:04 +0200

    Removed multiple worker threads. It seems that cost of thread synchronization and cash pollution caused by use of multiple threads were making it run slower not faster. Now it runs 30-40% faster.

--- http_routing.c
+++ http_routing.c
@@ -13,6 +13,7 @@ void route_http_request(Http_request_context_t* req_con, Http_response_t* res) {
         exit(1);
     }
     strncpy(path, req_con->uri.path.cstr, req_con->uri.path.len);
+    path[req_con->uri.path.len] = '\0';
     ssize_t ind = resource_index_for_path(path);
     res->resource_index = ind;
 }
--- simple_http_server.c
+++ simple_http_server.c
@@ -9,16 +9,69 @@
 
 #include "log.h"
 #include "tcp_connection.h"
-#include "http_request_queue.h"
 #include "http_resources.h"
+#include "http_request_context.h"
+#include "http_response.h"
+#include "reader.h"
 
-int g_workers_finished = 0;
-cnd_t g_cnd_worker_finished;
 char g_www_root[PATH_MAX];
 size_t g_root_len;
 
 struct in_addr addr;
 uint16_t port = 0;
+
+void echo_request_response_pair(Http_request_context_t* req_con, Http_response_t* res);
+void process_command_line_arguments(int argc, char** argv);
+
+int main(int argc, char** argv) {
+    process_command_line_arguments(argc, argv);
+    init_resources_from_config();
+    //Do not wait for children return values, prevents zombie processes
+    signal(SIGCHLD, SIG_IGN);
+
+    //Listen for tcp connection
+    int tcp_listener_fd = create_tcp_listener(addr.s_addr, port);
+    Tcp_connection_t tcp_connection;
+    while (1) {
+        tcp_connection = accept_tcp_connection(tcp_listener_fd);
+        if (fork()) { //It should be a thread pool instead of fork but ...
+            close_tcp_connection(&tcp_connection);
+            continue; //Parent keeps listening
+        }
+        else {
+            close_tcp_listener(tcp_listener_fd);
+            break; //Child deals with the established connection
+        }
+    }
+
+    Http_request_context_t req_con;
+    init_request_context(&req_con);
+    while (!is_reading_finished(tcp_connection)) {
+        req_con.status = parse_http_request(&req_con.req, tcp_connection);
+        if (req_con.status == PARSING_BROKEN_CLOSE_CONNECTION) {
+            abort_reading(tcp_connection);
+        }
+        req_con.status == process_request(&req_con);
+        Http_response_t res;
+        init_response(&res);
+        req_con.status = prepare_response(&res, &req_con);
+        if (req_con.status != PARSING_BROKEN_CLOSE_CONNECTION) {
+            send_response(&res, tcp_connection);
+            DEBUG(echo_request_response_pair(&req_con, &res));
+        }
+        else {
+            LOG(INFO, "Parsing broken, closing connection!");
+        }
+        free_response(&res);
+        clean_request_context(&req_con);
+    }
+
+    //Cleanup
+    free_request_context(&req_con);
+    close_tcp_connection(&tcp_connection);
+    fflush(stdout);
+    return 0;
+}
 
 void process_command_line_arguments(int argc, char** argv) {
     if (argc != 4) {
@@ -61,60 +114,10 @@ void process_command_line_arguments(int argc, char** argv) {
     port = htons(port);
 }
 
-int main(int argc, char** argv) {
-    process_command_line_arguments(argc, argv);
-    init_resources_from_config();
-    //Do not wait for children return values, prevents zombie processes
-    signal(SIGCHLD, SIG_IGN);
-
-    //Listen for tcp connection
-    int tcp_listener_fd = create_tcp_listener(addr.s_addr, port);
-    Tcp_connection_t tcp_connection;
-    while (1) {
-        tcp_connection = accept_tcp_connection(tcp_listener_fd);
-        if (fork()) { //It should be a thread pool instead of fork but ...
-            close_tcp_connection(&tcp_connection);
-            continue; //Parent keeps listening
-        }
-        else {
-            close_tcp_listener(tcp_listener_fd);
-            break; //Child deals with the established connection
-        }
-    }
-
-    Request_queue_t rq;
-    init_request_queue(&rq);
-    if (cnd_init(&g_cnd_worker_finished) != thrd_success) {
-        LOG(ERROR, "cnd_init failed!");
-        exit(1);
-    }
-
-    //Start worker threads writing and sending responses
-#define NUMBER_OF_WRITERS 4
-    if (NUMBER_OF_WRITERS > REQUEST_QUEUE_CAPACITY) {
-        LOG(ERROR, "Too many writers!");
-        exit(1);
-    }
-    init_writers(&rq, NUMBER_OF_WRITERS, tcp_connection);
-
-    //Loop inside request queue menager and parse requests as long as the connection is active
-    request_queue_manager(&rq, tcp_connection);
-
-    //Wait for all workers to finish
-    if (mtx_lock(&rq.mtx) == thrd_error) {
-        LOG(ERROR, "mtx_lock failed!");
-        exit(1);
-    }
-    while (g_workers_finished < NUMBER_OF_WRITERS) {
-        cnd_wait(&g_cnd_worker_finished, &rq.mtx);
-    }
-    mtx_unlock(&rq.mtx);
-
-    //Cleanup
-    cnd_destroy(&g_cnd_worker_finished);
-    free_request_queue(&rq);
-    close_tcp_connection(&tcp_connection);
-    fflush(stdout);
-    return 0;
+void echo_request_response_pair(Http_request_context_t* req_con, Http_response_t* res) {
+    printf("---request----\n");
+    print_request_context(req_con);
+    printf("---response---\n");
+    print_response(res);
 }
 
